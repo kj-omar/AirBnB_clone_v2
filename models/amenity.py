fb43f6fa83b8bbb80e5147f1fd285e4f348e7d25
Oliver Samuel <oliversamuel146@gmail.com>
2022-12-03 17:47:48 +0100

    corrected

--- models/amenity.py
+++ models/amenity.py
@@ -1,21 +1,14 @@
-#!/usr/bin/python
-""" holds class Amenity"""
-import models
-from models.base_model import BaseModel, Base
-from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+#!/usr/bin/python3
+"""
+Amenity class, a subclass of BaseModel
+"""
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """Representation of Amenity """
-    if models.storage_t == 'db':
-        __tablename__ = 'amenities'
-        name = Column(String(128), nullable=False)
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes Amenity"""
-        super().__init__(*args, **kwargs)
+class Amenity(BaseModel):
+    """
+    A subclass of BaseModel class
+    Public class attribute:
+        name: (str)
+    """
+    name = ""
